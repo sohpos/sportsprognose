121b6537777c5a537c3ad718cc51c03ba1999fb8
sohpos <sohpos@gmx.net>
2026-04-14 12:52:59 +0000

    Fix core type exports for bundler compatibility

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,9 +5,9 @@ export { SUPPORTED_LOCALES } from './i18n/LocaleManager';
 export { NAMESPACES } from './i18n/LocaleManager';
 export { t, d, n, p, c, o } from './i18n/LocaleManager';
 
-// Type exports
-export type { Locale } from './i18n/LocaleManager';
-export type { LocaleConfig } from './i18n/LocaleManager';
+// Type exports (use inline for bundler compatibility)
+import type { Locale, LocaleConfig } from './i18n/LocaleManager';
+export type { Locale, LocaleConfig };
 
 // Utils
 export * from './utils';
